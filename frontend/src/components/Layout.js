b58e59a7a5b5af9bde0e2790bf06992c239454b7
Anshul Kanwar <4n5hu1@protonmail.com>
2022-04-30 16:51:48 +0530

    Updated 404 Page

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,7 +1,7 @@
 const Layout = ({ title, children }) => {
   return (
     <div className="max-w-6xl mx-auto mt-20 px-5 sm:px-20">
-        <h1 className="text-3xl font-semibold mb-10">{title}</h1>
+        {title && <h1 className="text-3xl font-semibold mb-10">{title}</h1>}
         {children}
     </div>
   );
--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,6 +1,13 @@
+import Layout from '../components/Layout'
+
 const NotFoundPage = () => {
   return (
-    <div>404</div>
+    <Layout>
+      <div className='flex flex-col w-fit mx-auto gap-y-5'>
+        <h1 className='text-9xl font-black text-slate-500'>404</h1>
+        <p className='self-end text-2xl'>Not Found.</p>
+      </div>
+    </Layout>
   )
 }
 
