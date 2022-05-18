b8fe02cce110815876366716d63fa4ca5bad6259
Anshul Kanwar <4n5hu1@protonmail.com>
2022-05-19 01:49:49 +0530

    Removed authentications permission on CourseList view (temporary fix)

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,17 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 
 from .serializers import CourseSerializer
+from .models import Course
 
-from users.models import Student
+# from users.models import Student
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def courseList(request):
     user = request.user
-    student = Student.objects.get(user=user)
-    courses = student.course_set.all()
+    # student = Student.objects.get(user=user)
+    # courses = student.course_set.all()
+    courses = Course.objects.all()
     serializer = CourseSerializer(courses, many=True)
     return Response(serializer.data)
