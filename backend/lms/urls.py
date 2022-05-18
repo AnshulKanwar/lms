fdb3466f437ec1acef531153dd2346bfdbd6e08d
Anshul Kanwar <4n5hu1@protonmail.com>
2022-05-19 01:08:47 +0530

    Update Admin Panel

--- backend/lms/urls.py
+++ backend/lms/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header  =  "LMS Administration"  
+admin.site.site_title  =  "LMS Administration"
+admin.site.index_title  =  "LMS Administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
