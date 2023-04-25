3ba899971291ba281d5d5b268dcaacd28d38e555
Ojas1Sharma <74828090+Ojas1Sharma@users.noreply.github.com>
2023-04-26 01:52:48 +0530

    Update settings.py

--- backend/lms/settings.py
+++ backend/lms/settings.py
@@ -14,7 +14,8 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY2="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+SECRET_KEY = SECRET_KEY2
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
